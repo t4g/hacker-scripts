081471b0c49847ab1bef4cbe8d593b4acd47351e
Kyle <t4g@users.noreply.github.com>
2015-11-24 16:34:31 +0200

    Corrected filename of nested executable.
    ** Fixed nested execution path to match the actual folder structure.
    ** Allowed the scripts to be run without renaming and moving to a path directory.
    ** Made a generic bash shim that doesn't require adjustments or hard-coding to execute simulary named ruby script.

--- kumar-asshole.sh
+++ kumar-asshole.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 #
-# Requires kumar_asshole script in your bin
+# Requires the mentioned script to be in your bin AND renamed to not include the extension 
+# or ---- BUGFIXED --- it will just work if run from the directory as supplied by us. :p
 #
 
-exec kumar_asshole
+DI=$(cd "`dirname $0`" && pwd);
+EX=$(echo `basename $0` | sed -E "s/(.+)\.[^.]+$/\1/");
+
+which "${EX}" && "${EX}" \
+   || exec "${EX}.rb" \
+   || printf "binary <%s> not located." "${EX}">&2;
+
+cd -;
